581473148bac58717744e2a2c50a399bada65743
yillkid <yillkid@gmail.com>
2020-05-01 22:47:37 +0800

    feat: dict

---
+++ sample/dict.py
@@ -0,0 +1,11 @@
+student = {}
+
+student["name"] = "黃俊毓"
+student["number"] = "3"
+student["subject"] = "數學"
+student["score"] = "60"
+
+print("姓名：" +  student["name"])
+print("座號：" +  student["number"])
+print("科目：" +  student["subject"])
+print("分數：" +  student["score"])
