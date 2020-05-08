44a0e2edfeecf28cbed5fe2172379b4e9bbb7da0
yillkid <yillkid@gmail.com>
2020-05-08 15:58:51 +0800

    feat: workshop - import

---
+++ workshop/2020-05-09/import/brain.py
@@ -0,0 +1,8 @@
+def guess(number):
+    if number == 5:
+       return "答對了"
+
+    if number < 10:
+        return "太小了!"
+    else:
+        return "太大了！"
---
+++ workshop/2020-05-09/import/play.py
@@ -0,0 +1,5 @@
+from brain import guess
+
+number = int(input("請猜數字: "))
+result = guess(number)
+print(result)
