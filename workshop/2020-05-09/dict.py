484fafe5e9f362c906fe867fe1f555d61cd326a0
yillkid <yillkid@gmail.com>
2020-05-08 15:34:34 +0800

    feat: add workshop: dict

---
+++ workshop/2020-05-09/dict.py
@@ -0,0 +1,10 @@
+def pass_check(student):
+    if 這邊要寫什麼？ >= 60:
+        return "及格"
+    else:
+        return "不及格"
+
+s001 = {"name": "小花", "年級": "1年甲班", "分數":30}
+
+result = pass_check(s001)
+print(result)
